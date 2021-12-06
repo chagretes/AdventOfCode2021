dc8c702b9262b77ed3c0341b3210611ae4a4c2b8
Felipe Chagas <felchagas@hotmail.com>
2021-12-06 19:00:43 -0300

    Day -5

---
+++ AdventOfCode/Day_04.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using AoCHelper;
+
+namespace AdventOfCode
+{
+    class Square {
+        public int value;
+        public bool check = false;
+    }
+    class Board {
+        public bool won = false;
+        public List<List<Square>> rows = new List<List<Square>>();
+        public List<Square> lineChecked = null;
+
+        internal void WriteNumber(int number)
+        {
+            foreach(var row in rows) {
+                for(int i = 0; i<row.Count; i++)
+                {
+                    if (row[i].value==number) row[i].check = true;
+                    if(row.FindAll(x => x.check).Count == row.Count) {
+                        lineChecked = row;
+                    } else {
+                        List<Square> col = new List<Square> {
+                            rows[0][i],
+                            rows[1][i],
+                            rows[2][i],
+                            rows[3][i],
+                            rows[4][i],
+                        };
+                        if(col.FindAll(x => x.check).Count == col.Count) {
+                            lineChecked = col;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    public class Day_04 : BaseDay
+    {
+        private readonly List<int> numbers;
+        private readonly List<Board> boards = new List<Board>();
+
+        public Day_04()
+        {
+            var input = File.ReadAllText(InputFilePath).Split('\n').ToList();
+            numbers = input[0].Split(',').Select(int.Parse).ToList();
+            for(int i=2;i<input.Count-4;i+=6) {
+                var board = new Board{
+                    rows = new List<List<Square>>(){
+                        input[i].TrimStart().Replace("  "," ").Split(' ').Select(x => new Square{value = int.Parse(x),check = false}).ToList(),
+                        input[i+1].TrimStart().Replace("  "," ").Split(' ').Select(x => new Square{value = int.Parse(x),check = false}).ToList(),
+                        input[i+2].TrimStart().Replace("  "," ").Split(' ').Select(x => new Square{value = int.Parse(x),check = false}).ToList(),
+                        input[i+3].TrimStart().Replace("  "," ").Split(' ').Select(x => new Square{value = int.Parse(x),check = false}).ToList(),
+                        input[i+4].TrimStart().Replace("  "," ").Split(' ').Select(x => new Square{value = int.Parse(x),check = false}).ToList()
+                    }
+                };
+                boards.Add(board);
+            }
+        }
+
+        public override ValueTask<string> Solve_1() {
+            int result = 0;
+            foreach(var number in numbers){
+                foreach(var board in boards){
+                    board.WriteNumber(number);
+                    if(board.lineChecked!=null) {
+                        var sum = board.rows.Sum(x => x.Sum(y => y.check?0:y.value));
+                        result = sum * number;
+                        break;
+                    }
+                }
+                if (result != 0) break;
+            }
+
+            return new ValueTask<string>(result.ToString());
+        }
+
+        public override ValueTask<string> Solve_2(){
+            int result = 0;
+            foreach(var number in numbers){
+                foreach(var board in boards){
+                    board.WriteNumber(number);
+                    if(board.lineChecked!=null) {
+                        if(board.won == false){
+                            var sum = board.rows.Sum(x => x.Sum(y => y.check?0:y.value));
+                            result = sum * number;
+                            board.won = true;
+                        }
+                    }
+                }
+            }
+
+            return new ValueTask<string>(result.ToString());
+        }
+    }
+}
---
+++ AdventOfCode/Day_05.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using AoCHelper;
+
+namespace AdventOfCode
+{
+    public class Line{
+        public int sx;
+        public int sy;
+        public int ex;
+        public int ey;
+    }
+    public class Day_05 : BaseDay
+    {
+        private readonly List<Line> _lines = new List<Line>();
+        private int[,] points = new int[999,999];
+        private int result;
+
+        public Day_05()
+        {
+            var input = File.ReadAllText(InputFilePath).Split('\n').ToList();
+            foreach(var line in input) {
+                _lines.Add(new Line {
+                    sx = int.Parse(line.Split(" -> ")[0].Split(',')[0]),
+                    sy = int.Parse(line.Split(" -> ")[0].Split(',')[1]),
+                    ex = int.Parse(line.Split(" -> ")[1].Split(',')[0]),
+                    ey = int.Parse(line.Split(" -> ")[1].Split(',')[1]),
+                });
+            }
+            
+        }
+
+        public override ValueTask<string> Solve_1()
+        {
+            result = 0;
+            foreach(var line in _lines) {
+                //if((line.ex == line.sx)||(line.sy == line.ey))
+                if(line.ex == line.sx) {
+                    int x = line.ex;
+                    int maxy = line.sy > line.ey ? line.sy : line.ey;
+                    int miny = maxy == line.sy ? line.ey: line.sy ;
+                    for(int y = miny;y<=maxy;y++){
+                        points[x,y] = points[x,y] + 1;
+                        if(points[x,y]==2) 
+                            result++;
+                    }
+                }
+                else if (line.ey == line.sy) {
+                    int y = line.ey;
+                    int maxx = line.sx > line.ex ? line.sx : line.ex;
+                    int minx = maxx == line.sx ? line.ex : line.sx;
+                    for(int x = minx;x<=maxx;x++){
+                        points[x,y] = points[x,y] + 1;
+                        if(points[x,y]==2) 
+                            result++;
+                    }
+                }
+            }
+
+            return new ValueTask<string>(result.ToString());
+        }
+
+        public override ValueTask<string> Solve_2()
+        {
+            foreach(var line in _lines) {
+                if(line.ex != line.sx && line.ey != line.sy)
+                {
+                    int x = line.sx;
+                    int y = line.sy;
+                    int var_x = line.sx < line.ex ? 1 : -1;
+                    int var_y = line.sy < line.ey ? 1 : -1;
+                    do {
+                        points[x,y] = points[x,y] + 1;
+                        if(points[x,y]==2) 
+                            result++;
+                        x = x + var_x;
+                        y = y + var_y;
+                    }while(x!=line.ex&&y!=line.ey);
+
+                } else if (line.ex == line.sx && line.ey==line.sy)
+                {
+                    Console.WriteLine("asd");
+                }
+                
+            }
+
+            return new ValueTask<string>(result.ToString());
+        }
+    }
+}
